ffdbbd9b3ec77bf45de4fbd2af62bf58a06c2588
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-04-16 16:14:25 +1000

    Update copyright statement to adopt the project license

--- packages/nci_site_archive/src/site_archive_nci/utilities.py
+++ packages/nci_site_archive/src/site_archive_nci/utilities.py
@@ -1,10 +1,16 @@
 # Copyright Commonwealth of Australia, Bureau of Meteorology 2024.
-# This software is provided under license 'as is', without warranty
-# of any kind including, but not limited to, fitness for a particular
-# purpose. The user assumes the entire risk as to the use and
-# performance of the software. In no event shall the copyright holder
-# be held liable for any claim, damages or other liability arising
-# from the use of the software.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Utilities for NCI indexes"""
 
