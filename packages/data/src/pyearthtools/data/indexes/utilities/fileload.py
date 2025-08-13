bbdc7397a24818c29302558d66ca93caaf7e3d66
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2025-08-13 10:40:48 +1000

    Modify file load defaults to account for change in upstream package defaults

--- packages/data/src/pyearthtools/data/indexes/utilities/fileload.py
+++ packages/data/src/pyearthtools/data/indexes/utilities/fileload.py
@@ -80,8 +80,9 @@ def open_dataset(
 
     if isinstance(location, (tuple, list)):
         try:
-            ds = xr.open_mfdataset(filter_files(location), **get_config(True))
-            return ds
+            return xr.open_mfdataset(filter_files(location), 
+            decode_timedelta=True,  # TODO: should we raise a warning? It seems to be required for almost all our data.
+            **get_config(True))
 
         except xr.MergeError as e:
             if not soft_fail:
